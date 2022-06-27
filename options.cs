4ffafff518353a6506db81b3ddabf54e6bdefa7d
Matteo Cellucci <matteo.cellucci@keypartner.com>
2022-06-27 09:46:59 +0200

    Sftp test (#1)
    * chore: Copy all files into this repository
    
    * feat Add pipeline
    
    Co-authored-by: Pasquale De Vita <59291437+pasqualedevita@users.noreply.github.com>

---
+++ options.cs
@@ -0,0 +1,12 @@
+namespace PagoPA
+{
+  public class Options
+  {
+    public string FunctionName { get; set; }
+    public string Region { get; set; }
+    public int FileSizeInMb { get; set; }
+    public string SftpHost { get; set; }
+    public string SftpUsername { get; set; }
+    public string SftpPassword { get; set; }
+  }
+}
